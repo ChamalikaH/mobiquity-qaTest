125ef1e61685db4d0d0e70633f34fdff2056daeb
Chamalika Hewawitharana <chamalika@dolphiq.nl>
2022-03-18 12:47:14 +0530

    add properties and pom changes

---
+++ src/main/kotlin/com/jsonplaceholder/typicode/TypicodeApplication.kt
@@ -0,0 +1,11 @@
+package com.jsonplaceholder.typicode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TypicodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TypicodeApplication>(*args)
+}
---
+++ src/test/kotlin/com/jsonplaceholder/typicode/TypicodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jsonplaceholder.typicode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+public class TypicodeApplicationTests {
+
+	/*@Test
+	fun contextLoads() {
+	}*/
+
+}
